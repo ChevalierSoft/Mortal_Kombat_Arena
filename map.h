5f44260e5ce0fac5d88497287db9753f73c034ad
Eldariur <alzi0masta@gmail.com>
2019-01-23 17:20:00 +0100

    Oups Changement pointeur struct void -> mage-t

--- map.h
+++ map.h
@@ -2,12 +2,13 @@
 #define H_M
 
 #include "fonctions.h"
+#include "personnage.h"
 
 #define N 10
 
 typedef struct carellage_s{
   int land;
-  void * personnage;
+  mage_t * personnage;
 }carellage_t;
 
 void init_map(carellage_t map[N][N]);
