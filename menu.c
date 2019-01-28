02124083178e6b9cd6604495ea3296590d13a6bd
ChevalierSoft <ibanarashi@outlook.fr>
2019-01-28 16:54:09 +0100

    wé c kool

--- menu.c
+++ menu.c
@@ -5,27 +5,61 @@ void pas_fini(){
   printf(YEL"%s\n", "sort non castable avant la prochaine lune \n"RESET);
 }
 
-void menu_choix(mage_t * m, carellage_t map[N][N]){
+void cast_mage_noir(mage_t * m, carellage_t map[N][N]){
   int choix=-1;
-    printf(MAG"+++++++++++++++++++\n" RESET);
-    printf(MAG"%s\n" RESET, m->nom);
-    printf(MAG"+++++++++++++++++++\n" RESET);
-    printf(RED"+++++++++++++++++++\n" );
-    printf("+ 1) boule de feu \n");
-    printf("+ 2) make america great again \n");
-    printf("+ 3) passer le tour \n");
-    printf("++++++++++++++++++\n" RESET);
 
-    while (choix <1 || choix >3) {
-      scanf("%d",&choix);
-    }
+  printf(MAG"+++++++++++++++++++\n" RESET);
+  printf(MAG"%s\n" RESET, m->nom);
+  printf(MAG"+++++++++++++++++++\n" RESET);
+  printf(RED"+++++++++++++++++++\n" );
+  printf("+ 1) boule de feu \n");
+  printf("+ 2) make america great again \n");
+  printf("+ 3) passer le tour \n");
+  printf("++++++++++++++++++\n" RESET);
 
-    switch(choix){
-      case 1: boule_de_feu(m,map);break;
-      case 2: pas_fini();break;
-      case 3: printf("%s passe son tour", m->nom);break;
-      default: pas_fini();break;
-    }
+  while (choix <1 || choix >3) {
+    scanf("%d",&choix);
+  }
+
+  switch(choix){
+    case 1: boule_de_feu(m,map);break;
+    case 2: pas_fini();break;
+    case 3: printf("%s passe son tour", m->nom);break;
+    default: pas_fini();break;
+  }
+}
+
+void cast_mage_blanc(mage_t * m, carellage_t map[N][N]){
+  int choix=-1;
+  printf(MAG"+++++++++++++++++++\n" RESET);
+  printf(MAG"%s\n" RESET, m->nom);
+  printf(MAG"+++++++++++++++++++\n" RESET);
+  printf(RED"+++++++++++++++++++\n" );
+  printf("+ 1) soin \n");
+  printf("+ 2) antidote \n");
+  printf("+ 3) passer le tour \n");
+  printf("++++++++++++++++++\n" RESET);
+
+  while (choix <1 || choix >3) {
+    scanf("%d",&choix);
+  }
+
+  switch(choix){
+    case 1: pas_fini();break;
+    case 2: pas_fini();break;
+    case 3: printf("%s passe son tour", m->nom);break;
+    default: pas_fini();break;
+  }
+}
+
+void menu_choix(mage_t * m, carellage_t map[N][N]){
+
+  switch (m->classe) {
+    case mage_noir: cast_mage_noir(m,map); break;
+    case mage_blanc: cast_mage_blanc(m,map); break;
+    default: printf(YEL"\nclasse non répertorié\ndsl\n" RESET );
+  }
+
 
 }
 
