b996ee2808a0884e9da84aefaba4017e40df8eda
ChevalierSoft <root@vps597414.ovh.net>
2019-04-09 22:44:12 +0200

    base du protocole réseau. S'arrete juste apres la comparaison des hash.

---
+++ prgrammes de teste/reseau/client.c
@@ -0,0 +1,229 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <openssl/md5.h>
+
+#define H_COLOR
+#define BLK   "\x1B[30m"
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
+
+//#define SERVEURNAME "192.168.1.106" // adresse IP de mon serveur
+#define SERVEURNAME "127.0.1.1" // adresse IP de mon serveur
+#define client_id "Ash"
+#define QUITTER "QUITTER"
+
+char menu(){
+	char choix;
+	printf("Que voulez-vous faire ?\n");
+	printf("m: envoyer un message au serveur\n");
+	printf("q: quitter\n");
+	printf("Que voulez-vous faire ?\n");
+	scanf(" %c", &choix);
+	return choix;
+}
+
+void envoyer_message(int to_server_socket){
+	char msg[200], buffer[512];
+	printf("quel est votre message : ");
+	scanf(" %[^\n]s", buffer);
+	sprintf(msg, "MSG %s", buffer);
+	send(to_server_socket, msg, strlen(msg), 0); //on augmente la taille de 4 pour l'entête
+	// lecture de la réponse
+	memset(buffer, 0, sizeof(buffer));
+	recv(to_server_socket,buffer,512,0);
+	printf(MAG"[client] reponse du serveur : '%s'\n"RESET, buffer);
+	/*if(strncmp("MSG", buffer, 3)==0){
+			printf("[serveur] message reçu : '%s'\n",buffer+4);
+			//printf("[serveur] envoi de la réponse ");
+			//sprintf(buffer,"REPONSE DU SERVEUR");
+			envoyer_message(client_socket);
+			//send(client_socket, buffer, 512, 0);
+	}*/
+}
+
+void quitter(int to_server_socket){
+	printf("[client] envoi message QUITTER au serveur\n");
+	send(to_server_socket,QUITTER,7,0);
+}
+
+char *str2md5(const char * str, int length) {
+    int n;
+    MD5_CTX c;
+    unsigned char digest[16];
+    char *out = (char*)malloc(33);
+
+    MD5_Init(&c);
+
+    while (length > 0) {
+        if (length > 512) {
+            MD5_Update(&c, str, 512);
+        } else {
+            MD5_Update(&c, str, length);
+        }
+        length -= 512;
+        str += 512;
+    }
+
+    MD5_Final(digest, &c);
+
+    for (n = 0; n < 16; ++n) {
+        snprintf(&(out[n*2]), 16*2, "%02x", (unsigned int)digest[n]);
+    }
+
+    return out;
+}
+
+char * f2s(const char * file_name) {
+    
+    char * file_contents;    // fichier chargé
+    long input_file_size=1000; //c'est zéro mais bon...
+    FILE * input_file = fopen(file_name, "rb");
+    //printf("->%s\n", file_name);
+    fseek(input_file, 0, SEEK_END);
+    input_file_size = ftell(input_file);
+    rewind(input_file);
+    file_contents = malloc(input_file_size * (sizeof(char)));
+    fread(file_contents, sizeof(char), input_file_size, input_file);
+    //printf("%s\n",file_contents );
+    fclose(input_file);
+    return(file_contents);
+}
+
+char * get_md5(char * file_name){
+
+  char * file_contents = f2s(file_name);
+
+  char * output = str2md5(file_contents, strlen(file_contents));
+  //printf("%s\n", output);
+  //free(output);
+  free(file_contents);
+  
+  return(output);
+}
+
+int main (  int argc, char** argv ){
+
+	struct sockaddr_in serveur_addr;
+	struct hostent *serveur_info;
+	long hostAddr;
+	char buffer[512];
+	int to_server_socket;
+	int port = 30407;
+
+	bzero(&serveur_addr,sizeof(serveur_addr));
+	hostAddr = inet_addr(SERVEURNAME);
+	if ( (long)hostAddr != (long)-1 ){
+		bcopy(&hostAddr,&serveur_addr.sin_addr,sizeof(hostAddr));
+	}
+	else {
+		serveur_info = gethostbyname(SERVEURNAME);
+	  	if (serveur_info == NULL) {
+			printf("Impossible de récupérer les infos du serveur\n");
+			exit(0);
+	  	}
+	  	bcopy(serveur_info->h_addr,&serveur_addr.sin_addr,serveur_info->h_length);
+	}
+	serveur_addr.sin_port = htons(port);
+	serveur_addr.sin_family = AF_INET;
+	/* creation de la socket */
+	if ( (to_server_socket = socket(AF_INET,SOCK_STREAM,0)) < 0) {
+		printf("Impossible de créer la socket client\n");
+	  	exit(0);
+	}
+	/* requete de connexion */
+	if(connect( to_server_socket, (struct sockaddr *)&serveur_addr, sizeof(serveur_addr)) < 0 ) {
+		printf("Impossible de se connecter au serveur\n");
+	  	exit(0);
+	}
+	/* envoie de données et reception */
+
+
+/*---------------------------------------------------------------------------------------*/
+	//printf("port : %d\n",port);
+
+	//connection !
+	sprintf(buffer, "Connection client");
+	send(to_server_socket, buffer, strlen(buffer),0);
+
+	//nom du serveur?
+	memset(buffer, 0, sizeof(buffer));
+	recv(to_server_socket, buffer, 512, 0);
+	printf(YEL"[serveur] %s \n"RESET, buffer);
+	char server_id[512];
+	strcpy(server_id, buffer);
+
+	//nom client !
+	memset(buffer, 0, sizeof(buffer));
+	send(to_server_socket, client_id, strlen(client_id),0);
+
+	//Ready ?
+	memset(buffer, 0, sizeof(buffer));
+	recv(to_server_socket, buffer, 512, 0);
+	printf(YEL"[%s] %s \n"RESET, server_id, buffer);
+
+	//Ready !
+	memset(buffer, 0, sizeof(buffer));
+	printf("Pret ? (o|y)\n");
+  getchar();
+  sprintf(buffer,"Client pret");
+  send(to_server_socket, buffer, strlen(buffer),0);
+
+  //nom fichier save à md5 ?
+  memset(buffer, 0, sizeof(buffer));
+	recv(to_server_socket, buffer, 512, 0);
+	printf(YEL"[%s] %s \n"RESET, server_id, buffer);
+	char nomFichier[512];// = "p_save.txt";
+  strncpy(nomFichier, buffer, 512);
+  //printf("fichier a charger : %s\n", nomFichier);
+
+	//md5 !
+	char * client_md5 = get_md5(nomFichier);
+	printf("client_md5 : %s\n", client_md5);
+
+	memset(buffer, 0, sizeof(buffer));
+	sprintf(buffer, client_md5);
+	send(to_server_socket, buffer, strlen(buffer),0);
+	free(client_md5); //hé oui, faut free
+  
+  //Start
+  printf(RED"La partie commence\n"RESET);
+
+
+	//
+    /* Un menu pour faire differentes actions */
+	/*char choix;
+	do {
+		choix = menu();
+		switch(choix){
+			case 'm':
+				envoyer_message(to_server_socket);
+				break;
+			case 'q':
+				quitter(to_server_socket);
+				break;
+			default:
+				printf("Commande '%c' invalide... recommencez\n", choix);
+				break;
+		}
+
+	} while (choix != 'q');
+*/
+	/* fermeture de la connexion */
+	shutdown(to_server_socket,2);
+	close(to_server_socket);
+	return 0;
+}
---
+++ prgrammes de teste/reseau/serveur.c
@@ -0,0 +1,272 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <signal.h>
+#include <unistd.h>
+#include <openssl/md5.h>
+
+#define BLK   "\x1B[30m"
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
+
+//char * serveur_id = "DaBoi";
+#define serveur_id "DaBoi"
+
+char buffer[512];
+
+void fin(int sig){
+	printf("fin du serveur");
+}
+
+int hostname_to_ip(char * hostname , char* ip){
+
+    struct hostent *he;
+    struct in_addr **addr_list;
+    int i;
+
+    if ( (he = gethostbyname( hostname ) ) == NULL)
+    {
+        // get the host info
+        herror("gethostbyname");
+        return 1;
+    }
+
+    addr_list = (struct in_addr **) he->h_addr_list;
+
+    for(i = 0; addr_list[i] != NULL; i++)
+    {
+        //Return the first one;
+        strcpy(ip , inet_ntoa(*addr_list[i]) );
+        return 0;
+    }
+
+    return 1;
+}
+
+void view_ip(){
+  char s[256];
+  struct hostent *host;
+  struct in_addr **adr;
+  if (!gethostname(s, 256))
+    if ((host = gethostbyname(s)) != NULL)
+      for (adr = (struct in_addr **)host->h_addr_list; *adr; adr++)
+          printf("IP : %s\n", inet_ntoa(**adr));
+}
+
+void envoyer_message(int client_socket){
+	char msg[200], buffer[512];
+	printf("quel est votre message de reponse : ");
+	scanf(" %[^\n]s", buffer);
+	sprintf(msg, "MSG %s", buffer);
+	send(client_socket, msg, strlen(msg), 0); //on augmente la taille de 4 pour l'entête
+	// lecture de la réponse
+	memset(buffer, 0, sizeof(buffer));
+	recv(client_socket,buffer,512,0);
+	printf("[client] reponse du serveur : '%s'\n", buffer);
+}
+/*
+void quick_send(int client_socket){
+	char msg[200], buffer[512];
+	printf("quel est votre message de reponse : ");
+	scanf(" %[^\n]s", buffer);
+	sprintf(msg, "MSG %s", buffer);
+	send(client_socket, msg, strlen(msg), 0); //on augmente la taille de 4 pour l'entête
+	// lecture de la réponse
+	memset(buffer, 0, sizeof(buffer));
+	recv(client_socket,buffer,512,0);
+	printf("[client] reponse du serveur : '%s'\n", buffer);
+}
+*/
+char *str2md5(const char * str, int length) {
+    int n;
+    MD5_CTX c;
+    unsigned char digest[16];
+    char *out = (char*)malloc(33);
+
+    MD5_Init(&c);
+
+    while (length > 0) {
+        if (length > 512) {
+            MD5_Update(&c, str, 512);
+        } else {
+            MD5_Update(&c, str, length);
+        }
+        length -= 512;
+        str += 512;
+    }
+
+    MD5_Final(digest, &c);
+
+    for (n = 0; n < 16; ++n) {
+        snprintf(&(out[n*2]), 16*2, "%02x", (unsigned int)digest[n]);
+    }
+
+    return out;
+}
+
+char * f2s(const char * file_name) {
+    
+    char * file_contents;    // fichier chargé
+    long input_file_size=1000; //c'est zéro mais bon...
+    FILE * input_file = fopen(file_name, "rb");
+    //printf("->%s\n", file_name);
+    fseek(input_file, 0, SEEK_END);
+    input_file_size = ftell(input_file);
+    rewind(input_file);
+    file_contents = malloc(input_file_size * (sizeof(char)));
+    fread(file_contents, sizeof(char), input_file_size, input_file);
+    //printf("%s\n",file_contents );
+    fclose(input_file);
+    return(file_contents);
+}
+
+char * get_md5(char * file_name){
+
+  char * file_contents = f2s(file_name);
+
+  char * output = str2md5(file_contents, strlen(file_contents));
+  //printf("%s\n", output);
+  //free(output);
+  free(file_contents);
+  
+  return(output);
+}
+
+int main ( void ){
+
+	int ma_socket;
+	int client_socket;
+	struct sockaddr_in mon_address, client_address;
+	unsigned int mon_address_longueur, lg;
+	bzero(&mon_address,sizeof(mon_address));
+	mon_address.sin_port = htons(30407);
+	mon_address.sin_family = AF_INET;
+	mon_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	char *hostname = "localhost";
+  char ip[100];
+
+  hostname_to_ip(hostname , ip);
+	fprintf(stderr, "%s resolved to %s" , hostname , ip);
+	view_ip();
+
+	/* creation de socket */
+	if ((ma_socket = socket(AF_INET,SOCK_STREAM,0))== -1) {
+		printf("Impossible de créer la socket\n");
+		exit(0);
+	}
+
+	/* bind serveur - socket */
+	bind(ma_socket,(struct sockaddr *)&mon_address,sizeof(mon_address));
+	/* ecoute sur la socket */
+	listen(ma_socket,5);
+	/* accept la connexion */
+	mon_address_longueur = sizeof(client_address);
+
+    /* on attend que le client se connecte */
+	client_socket = accept(ma_socket,
+                         (struct sockaddr *)&client_address,
+                         &mon_address_longueur);
+
+	//int quitter=0;
+
+  //while(!quitter){
+		memset(buffer, 0, sizeof(buffer));
+		recv(client_socket, buffer, 512,0);
+/*
+		if(strncmp("Connection", buffer, 10)==0){
+			printf(YEL"[serveur] message de connection reçu : '%s'\n"RESET,buffer);
+
+			//printf("[serveur] envoi de la réponse ");
+			//sprintf(buffer,"REPONSE DU SERVEUR");
+			//envoyer_message(client_socket);
+			send(client_socket, serveur_id, strlen(serveur_id), 0);
+			//send(client_socket, buffer, 512, 0);
+		} else if(strncmp("QUITTER", buffer, 7) == 0) {
+			printf("[serveur] déconnexion demandée : '%s'\n",buffer);
+			shutdown(client_socket,2);
+			close(client_socket);
+			quitter=1;
+		} else if(strncmp("BONJOUR", buffer, 7) == 0){
+			printf("[serveur] BONJOUR d'un client\n");
+			send(client_socket, "BONJOUR", 7, 0);
+		} else {
+			printf("[serveur] message inconnu : '%s'\n", buffer);
+		}*/
+
+		//if(strncmp("Connection", buffer, 10)==0){
+
+			//envoie du nom serveur
+      printf(YEL"[client] %s \n"RESET, buffer);
+      memset(buffer, 0, sizeof(buffer));
+			printf("envoie du serveur_id %s\n", serveur_id);
+			sprintf(buffer,"DaBoi");
+			send(client_socket, buffer, strlen(buffer), 0);
+
+			//reception du nom du client
+			memset(buffer, 0, sizeof(buffer));
+      recv(client_socket, buffer, 512,0);
+      char client_id[512];
+	    strcpy(client_id, buffer);
+      printf("client_id : %s \n" , client_id);
+
+      //Ready?
+      printf("Pret ? (o|y)\n");
+      getchar();
+      memset(buffer, 0, sizeof(buffer));
+			sprintf(buffer,"Ready ?");
+			send(client_socket, buffer, strlen(buffer), 0);
+
+			//Ready !
+			memset(buffer, 0, sizeof(buffer));
+      recv(client_socket, buffer, 512,0);
+      printf(YEL"[%s] %s \n"RESET, client_id, buffer);
+      
+      //nom fichier save à md5 ?
+      char * nomFichier = "p_save.txt";
+      memset(buffer, 0, sizeof(buffer));
+			sprintf(buffer, nomFichier);
+			send(client_socket, buffer, strlen(buffer), 0);
+
+
+			//md5 !
+      memset(buffer, 0, sizeof(buffer));
+      recv(client_socket, buffer, 512,0);
+      printf(YEL"[%s] %s \n"RESET, client_id, buffer);
+      printf(YEL"tst	\n"RESET);
+
+      char * serveur_md5 = get_md5(nomFichier);
+
+			printf("hash du fichier %s : %s\n", nomFichier, serveur_md5);
+      
+      if(! strcmp(buffer,serveur_md5))
+      	printf(CYN"Sauvegarde valide\n"RESET);
+      else
+      	printf(RED"Sauvegarde Erronee\n"RESET);
+
+			free(serveur_md5); //hé oui, faut free
+
+		  
+		  //Start
+		  printf(RED"La partie commence\n"RESET);
+
+  
+		//}
+
+	//printf("fin de boucle\n");
+	//}
+	shutdown(ma_socket,2);
+	close(ma_socket);
+	return 0;
+}
